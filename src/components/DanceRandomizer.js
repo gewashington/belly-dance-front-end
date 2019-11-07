f2028151be44b83599db9b2ff40affbcc79d8988
gewashington <gloriaewashington@gmail.com>
2019-11-06 21:35:54 -0500

    added border to navbar

--- src/components/DanceRandomizer.js
+++ src/components/DanceRandomizer.js
@@ -36,6 +36,7 @@ export default class DanceRandomizer extends React.Component {
     }
 
     onNumberChange = (e) => {
+        e.preventDefault();
         this.setState({
             number: e.target.value,
         })
